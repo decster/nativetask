a9375a5204261ef5198188755b2ed52b70a3c291
Binglin Chang <decstery@gmail.com>
2013-01-24 22:20:58 -0800

    update prebuild binaries

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,8 @@
 DISTNAME=nativetask-0.1.0
 DISTDIR=target/$DISTNAME
 mkdir -p $DISTDIR
-cp target/DISTNAME.jar $DISTDIR
-cp -r target/native/target/usr/include $DISTDIR/
-cp -r target/native/target/usr/lib $DISTDIR/
-cd target ; tar czf ../prebuild/$DISTNAME.tar.gz $DISTNAME ; cd -
+cp target/hadoop-$DISTNAME.jar $DISTDIR
+cp -r target/native/target/usr/local/include $DISTDIR/
+cp -r target/native/target/usr/local/lib $DISTDIR/
+cp INSTALL $DISTDIR/
+cd target ; rm ../prebuild/$DISTNAME.tar.gz ; tar czf ../prebuild/$DISTNAME.tar.gz $DISTNAME ; cd -
